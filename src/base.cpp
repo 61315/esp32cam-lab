5854e4687d9f728193bae6cddc8400d8950d1420
Minseo Park <studio61315@gmail.com>
2024-10-04 22:55:32 -0500

    chore: add formatter configs

--- src/base.cpp
+++ src/base.cpp
@@ -2,13 +2,16 @@
 
 #define CAMERA_MODEL_AI_THINKER
 
-int flashPin = 4;
+int        flashPin = 4;
+const char asd      = 3;
 
-void setup() { pinMode(flashPin, OUTPUT); }
+void setup() {
+    pinMode(flashPin, OUTPUT);
+}
 
 void loop() {
-  digitalWrite(flashPin, HIGH);
-  delay(1000);
-  digitalWrite(flashPin, LOW);
-  delay(1000);
+    digitalWrite(flashPin, HIGH);
+    delay(1000);
+    digitalWrite(flashPin, LOW);
+    delay(1000);
 }
